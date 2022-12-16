62659ea4fa6a4573d41c1ec80cc50f65b09acc95
MoizKhan1179 <120182920+MoizKhan1179@users.noreply.github.com>
2022-12-16 18:43:37 +0500

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,57 @@
+// let abc =["headphones",5560,"Assasins creed",true,false,undefined,NaN,90.87]
+
+// let abc=[];
+// abc [0]="PlayStation"
+// abc[2]=567
+// abc[4]=true
+// abc[6]=false
+
+
+
+// document.write(abc)
+// console.log(abc)
+
+
+
+// let products=["https://picsum.photos/200/300/?blur","https://picsum.photos/200/300?grayscale","https://picsum.photos/seed/picsum/200/300",]
+
+// for(i=0; i<=products.length ;i++){
+
+// document.write(`<img src="${products[i]}>`)
+// }
+
+// let userImage;
+// let products=[];
+
+
+// for(j=0; j<3; j++){
+//     let userImage= prompt("Enter Image URL");
+//     for(k=0; k<3; k++){
+//         products[j] = userImage
+//     }
+    
+// }
+
+
+// let products=[];
+
+// for(i=0; i<=products.length ;i++){
+
+//     document.write(`<img src="${products[i]}>`)
+//     }
+
+
+
+// let abc =["headphones",5560,"Assasins creed",true,false,undefined,NaN,90.87]
+
+// let bd = [];
+
+// bd = [ ...abc ]
+
+// console.log("abc 1st : " + abc)
+// console.log("bd 1st : " + bd)
+
+// bd[0]=42
+
+// console.log("abc 2st : " + bd)
+// console.log("bd 2st : " + bd)
