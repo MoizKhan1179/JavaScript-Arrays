43bd02abc2edf87288d505af76efc0f9a545388c
MoizKhan1179 <120182920+MoizKhan1179@users.noreply.github.com>
2022-12-19 18:24:08 +0500

    Add files via upload

--- Array.js
+++ Array.js
@@ -55,3 +55,149 @@
 
 // console.log("abc 2st : " + bd)
 // console.log("bd 2st : " + bd)
+
+
+
+// let numbers = [45,36,35,78,95,85]
+
+// console.log("before : " + numbers)
+
+// // numbers[1] = "Hello"
+
+// // numbers.push(45)  end ma insert
+
+// // numbers.pop(1)  end se delete
+
+// // delete numbers[3]
+
+// // document.write(numbers)
+
+// console.log("after : " + numbers)
+
+
+
+
+
+
+// ********array se pehly add kre ga***********
+
+
+
+
+// let numbers = [45,36,35,78,95,85]
+
+// console.log("before : " + numbers)
+
+// numbers.shift(4)
+
+// console.log("after : " + numbers)
+
+
+
+// let numbers = [45,36,35,78,95,85]
+
+// console.log("before : " + numbers)
+
+// numbers.unshift(99)
+
+
+// console.log("after : " + numbers)
+
+
+
+
+
+
+
+// ****SORT*****
+
+
+
+// let numbers = [45,36,5,35,10,10,9,78,95,85]
+// // let numbers = ["Arham","Shayan","Moiz","Fariz"]
+// console.log("before : " + numbers)
+
+// // numbers.sort()
+
+// // function
+
+// numbers.sort((a,b)=>{return a-b})
+
+
+// console.log("after : " + numbers)
+
+
+
+// *****SLICE******
+
+// let numbers = [45,36,35,78,95,85]
+
+// console.log("before : " + numbers)
+
+// let abc = numbers.slice(0,3)
+
+// console.log("after : " + numbers)
+
+// console.log("ABC : " + abc)
+
+
+// *****SPLICE******
+
+// let numbers = [45,36,35,7,78,95,8,85,2,11,22]
+
+// console.log("before : " + numbers)
+
+// numbers.splice(1,3)
+
+// console.log("after : " + numbers)
+
+// numbers.splice(4,3)
+
+// console.log("after1 : " + numbers)
+
+// *********ConCat********
+
+// let carArrays = [];
+// console.log("before : " + carArrays)
+
+// let carArrays2= ["Supra","Rx7","SkyLine"]
+let carArrays3= ["Supra22","Rx7V12","SkyLine1970"]
+// // let carArrays4 = ["Honda","Toyota","Bugatti","Lamborghini","Mazda"]
+
+// // carArrays = carArrays2.concat(carArrays3,carArrays4)
+
+// // console.log("after : " + carArrays)
+
+
+// // *****JOIN*****
+
+
+let Arrays3 = carArrays3.join("-")
+
+console.log(Arrays3)
+
+
+
+// ******IsArray****** array identify krne k lye
+
+// let car= ["Supra","Rx7","SkyLine","Cheverolette"]
+
+// console.log(car)
+
+
+// console.log(Array.isArray(car))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
